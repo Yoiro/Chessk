ec9b0777b70389e876378f592579a617a427b639
Unknown <la141353@student.helha.be>
2017-08-28 00:42:44 +0200

    add board

---
+++ Models/board.cpp
@@ -0,0 +1,11 @@
+#include "board.h"
+
+
+Board::Board(QObject *parent): QObject(parent)
+{
+    for(int i = 0; i < cases.length(); i++){
+        for(int j = 0; j < cases[i].length(); j++){
+            cases[i][j]
+        }
+    }
+}
