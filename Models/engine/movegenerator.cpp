af7d552975ed9824a07f308b7ff3f0299d1f48d7
Unknown <la141353@student.helha.be>
2017-08-28 05:01:29 +0200

    movegenerator if/elseif

--- Models/engine/movegenerator.cpp
+++ Models/engine/movegenerator.cpp
@@ -1,5 +1,12 @@
 #include "movegenerator.h"
 
+#include "Models/piece/king.h"
+#include "Models/piece/queen.h"
+#include "Models/piece/bishop.h"
+#include "Models/piece/knight.h"
+#include "Models/piece/pawn.h"
+#include "Models/piece/rook.h"
+
 MoveGenerator::MoveGenerator()
 {
 
@@ -7,5 +14,22 @@ MoveGenerator::MoveGenerator()
 
 void MoveGenerator::move(Piece *piece)
 {
+    if (King* k = dynamic_cast<King*>(piece)){
 
+    }
+    else if (Queen* q = dynamic_cast<Queen*>(piece)){
+
+    }
+    else if(Bishop* b = dynamic_cast<Bishop*>(piece)){
+
+    }
+    else if(Knight *k = dynamic_cast<Knight*>(piece)){
+
+    }
+    else if(Pawn* p = dynamic_cast<Pawn*>(piece)){
+
+    }
+    else if(Rook* r= dynamic_cast<Rook*>(piece)){
+
+    }
 }
